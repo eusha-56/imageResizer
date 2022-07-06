82b64cd4ebf4d9cbe87f180dabff192f93ee3eed
eusha-56 <66923941+eusha-56@users.noreply.github.com>
2022-07-06 15:19:22 +0600

    added new feature

--- main.py
+++ main.py
@@ -1,24 +1,27 @@
 import math
 import os
+import re
 from PIL import Image
+
 
 def resizeStyleFun():
     resizeStyle = input(
-'''
+        '''
 Resize according to:
 1. Width
 2. Height
 3. Percentage
+4. File size
 
 (Enter number. Press 0 to quit)\n
 '''
     )
-    
 
     while True:
         maxWidth = 0
         maxHeight = 0
         percentage = 100
+        fileSize = 0
 
         if resizeStyle == '0':
             break
@@ -32,29 +35,43 @@ Resize according to:
         elif resizeStyle == '3':
             percentage = input('Enter percentage from 1 to 100\n')
 
+        elif resizeStyle == '4':
+            fileSize = input('Enter approximate file size in MB\n')
+
         else:
             print('Enter NUMBER properly\n')
             return resizeStyleFun()
+
         rename = input(
-'''
+            '''
 Want to rename images?
 If yes, enter name otherwise leave blank.\n
 '''
-            )
-        return [rename,int(maxWidth),int(maxHeight),int(percentage)]
+        )
+        return [rename, int(maxWidth), int(maxHeight), float(percentage), float(fileSize)*1024*1024]
 
 
-def resize(originalName, newName, count, maxWidth, maxHeight, percentage):
+def resize(originalName, newName, count, maxWidth, maxHeight, percentage, modifiedFileSize):
+
     img = Image.open('inputImages/'+originalName)
+    originalFileSize = os.stat('inputImages/'+originalName).st_size
     width, height = img.size
-    area = width*height*percentage/100
+
+    if modifiedFileSize == 0 or modifiedFileSize > originalFileSize:
+        modifiedFileSize = originalFileSize
+
+    if percentage > 100:
+        percentage = 100
+
+    area = width*height*percentage/100*modifiedFileSize/originalFileSize
     aspectRatio = width/height
-    width = math.floor(math.sqrt(area*aspectRatio))
-    height = math.floor(width/aspectRatio)
-    
-    if maxWidth == 0:
+
+    width = math.ceil(math.sqrt(area*aspectRatio))
+    height = math.ceil(width/aspectRatio)
+
+    if maxWidth == 0 or maxWidth > width:
         maxWidth = width
-    if maxHeight == 0:
+    if maxHeight == 0 or maxHeight > height:
         maxHeight = height
     if newName == '':
         newName = originalName.split('.')[0]
@@ -62,25 +79,32 @@ def resize(originalName, newName, count, maxWidth, maxHeight, percentage):
     else:
         count = str(count)+'_'
 
-    img.thumbnail((maxWidth,maxHeight))
-    newImageName = 'outputImages/'+count+newName+'.'+originalName.split('.')[-1]
-    print('saving '+newImageName)
-    img.save(newImageName)
+    img.thumbnail((maxWidth, maxHeight))
+
+    newImageName = 'outputImages/'+count + \
+        newName+'.'+originalName.split('.')[-1]
+    print('saving ' + count + newName + '.' +
+          originalName.split('.')[-1] + ' in outputImages/')
+    print(width)
+    print(height)
+    img.save(newImageName, optimize=True, quality=85)
 
 
 def main():
-    imges =  os.listdir('inputImages')
+
+    files = os.listdir('inputImages')
+    imges = list(filter(lambda x: re.search(
+        '\.(jpe?g|png|gif|tiff)$', x), files))
     status = resizeStyleFun()
-    print(status)
 
     for i in range(len(imges)):
-        resize(imges[i], status[0], i+1, status[1], status[2], status[3])
-
+        resize(imges[i], status[0], i+1, status[1],
+               status[2], status[3], status[4])
 
 
 main()
 
+
 while True:
     if input('Successfully modified all images. Press enter to exit') == '':
         break
-
